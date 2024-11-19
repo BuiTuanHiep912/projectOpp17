a9228fc9761baa90a0d39dd8b1f7c128490ac4fe
hdkhanh <hoangduckhanhtn@gmail.com>
2024-11-19 17:16:13 +0700

    Tai len gameobjects

---
+++ src/main/java/seventeenth/group/gameobjects/Hero.java
@@ -0,0 +1,214 @@
+package seventeenth.group.gameobjects;
+
+import seventeenth.group.state.GameWorldState;
+import seventeenth.group.effect.Animation;
+import seventeenth.group.effect.CacheDataLoader;
+import java.applet.AudioClip;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public class Hero extends Human {
+    public static final int RUNSPEED = 3;
+
+    private Animation runForwardAnim, runBackAnim;
+    private Animation idleForwardAnim, idleBackAnim, idleShootingForwardAnim, idleShootingBackAnim;
+    private Animation deadForwardAnim, deadBackAnim;
+
+    private long lastShootingTime;
+    private boolean isShooting = false;
+
+    private AudioClip hurtingSound;
+    private AudioClip shooting1;
+
+    public Hero(float x, float y, GameWorldState gameWorld) {
+        super(x, y, 70, 90, 0.1f, 100, gameWorld);
+
+        shooting1 = CacheDataLoader.getInstance().getSound("");
+        hurtingSound = CacheDataLoader.getInstance().getSound("");
+
+        setTeamType(LEAGUE_TEAM);
+
+        setTimeForNoBehurt(2000*1000000); //thời gian miễn nhiễm sát thương (2s)
+
+
+        //lấy cái animation từ lớp CacheDataLoader
+        runForwardAnim = CacheDataLoader.getInstance().getAnimation("run");
+        runBackAnim = CacheDataLoader.getInstance().getAnimation("run");
+        runBackAnim.flipAllImage();
+
+        idleForwardAnim = CacheDataLoader.getInstance().getAnimation("idle_2");
+        idleBackAnim = CacheDataLoader.getInstance().getAnimation("idle_2");
+        idleBackAnim.flipAllImage();
+
+
+        behurtForwardAnim = CacheDataLoader.getInstance().getAnimation("hurt");
+        behurtBackAnim = CacheDataLoader.getInstance().getAnimation("hurt");
+        behurtBackAnim.flipAllImage();
+
+        idleShootingForwardAnim = CacheDataLoader.getInstance().getAnimation("shot_1");
+        idleShootingBackAnim = CacheDataLoader.getInstance().getAnimation("shot_1");
+        idleShootingBackAnim.flipAllImage();
+
+        deadForwardAnim = CacheDataLoader.getInstance().getAnimation("dead");
+        deadBackAnim = CacheDataLoader.getInstance().getAnimation("dead");
+        deadBackAnim.flipAllImage();
+
+
+
+    }
+
+    @Override
+    public void Update() {
+
+        super.Update();
+
+        if(isShooting){
+            if(System.nanoTime() - lastShootingTime > 500*1000000){
+                isShooting = false;
+            }
+        }
+    }
+
+    @Override
+    public Rectangle getBoundForCollisionWithEnemy() {
+        Rectangle rect = getBoundForCollisionWithMap();
+
+        rect.x = (int) getPosX() - 22;
+        rect.y = (int) getPosY() - 40;
+        rect.width = 44;
+        rect.height = 80;
+
+
+        return rect;
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+
+        switch(getState()){
+
+            case ALIVE:
+            case NOBEHURT:
+                if(getState() == NOBEHURT && (System.nanoTime()/10000000)%2!=1) //trạng thái nhân vật nhấp nháy miễn nhiễm sát thương
+                {
+                    System.out.println("Plash...");
+                }else{
+                    if(getSpeedX() > 0){
+                        runForwardAnim.Update(System.nanoTime());
+                        if(isShooting){
+                        }else
+                            runForwardAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
+                        if(runForwardAnim.getCurrentFrame() == 1) runForwardAnim.setIgnoreFrame(0);
+                    }else if(getSpeedX() < 0){
+                        runBackAnim.Update(System.nanoTime());
+                        if(isShooting){
+                        }else
+                            runBackAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
+                        if(runBackAnim.getCurrentFrame() == 1) runBackAnim.setIgnoreFrame(0);
+                    }else{
+                        if(getDirection() == RIGHT_DIR){
+                            if(isShooting){
+                                idleShootingForwardAnim.Update(System.nanoTime());
+                                idleShootingForwardAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
+                            }else{
+                                idleForwardAnim.Update(System.nanoTime());
+                                idleForwardAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
+                            }
+                        }else{
+                            if(isShooting){
+                                idleShootingBackAnim.Update(System.nanoTime());
+                                idleShootingBackAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
+                            }else{
+                                idleBackAnim.Update(System.nanoTime());
+                                idleBackAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
+                            }
+                        }
+                    }
+                }
+
+
+                break;
+
+            case BEHURT:
+                if(getDirection() == RIGHT_DIR){
+                    behurtForwardAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
+                }else{
+                    behurtBackAnim.setCurrentFrame(behurtForwardAnim.getCurrentFrame());
+                    behurtBackAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
+                }
+                break;
+
+            case FEY:
+
+                break;
+
+            case DEATH:
+                if(getBlood()==0 && getDirection() == RIGHT_DIR){
+                    deadForwardAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
+                }else{
+                    deadBackAnim.setCurrentFrame(deadForwardAnim.getCurrentFrame());
+                    deadBackAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
+                }
+                break;
+
+
+        }
+
+    }
+
+    @Override
+    public void run() {
+        if(getDirection() == LEFT_DIR)
+            setSpeedX(-3);
+        else setSpeedX(3);
+    }
+
+
+    @Override
+    public void stopRun() {
+        setSpeedX(0);
+        runForwardAnim.reset();
+        runBackAnim.reset();
+        runForwardAnim.unIgnoreFrame(0);
+        runBackAnim.unIgnoreFrame(0);
+    }
+
+    @Override
+    public void attack() {
+
+        if(!isShooting){
+
+            shooting1.play();
+
+            Bullet bullet = new BlueFire(getPosX(), getPosY(), getGameWorld());
+            if(getDirection() == LEFT_DIR) {
+                bullet.setSpeedX(-10);
+                bullet.setPosX(bullet.getPosX() - 40);
+                if(getSpeedX() != 0 && getSpeedY() == 0){
+                    bullet.setPosX(bullet.getPosX() - 10);
+                    bullet.setPosY(bullet.getPosY() - 5);
+                }
+            }else {
+                bullet.setSpeedX(10);
+                bullet.setPosX(bullet.getPosX() + 40);
+                if(getSpeedX() != 0 && getSpeedY() == 0){
+                    bullet.setPosX(bullet.getPosX() + 10);
+                    bullet.setPosY(bullet.getPosY() - 5);
+                }
+            }
+
+            bullet.setTeamType(getTeamType());
+            getGameWorld().bulletManager.addObject(bullet);
+
+            lastShootingTime = System.nanoTime();
+            isShooting = true;
+
+        }
+
+    }
+    @Override
+    public void hurtingCallback(){
+        System.out.println("Call back hurting");
+        hurtingSound.play();
+    }
+}
---
+++ src/main/java/seventeenth/group/gameobjects/ParticularObject.java
@@ -0,0 +1,264 @@
+package seventeenth.group.gameobjects;
+
+import seventeenth.group.state.GameWorldState;
+import seventeenth.group.effect.Animation;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+public abstract class ParticularObject extends GameObject{
+    public static final int LEAGUE_TEAM = 1;
+    //đối tượng cùng phe nhân vật chính
+    //khi chạm vào thì kh mất máu
+    public static final int ENEMY_TEAM = 2;
+    //ngược lại phía trên
+
+    // Các hằng số cho hướng di chuyển
+    public static final int LEFT_DIR = 0;
+    public static final int RIGHT_DIR = 1;
+    public static final int UP_DIR = 2;    // Thêm hằng số cho hướng lên
+    public static final int DOWN_DIR = 3;  // Thêm hằng số cho hướng xuống
+
+
+    public static final int ALIVE = 0;
+    public static final int BEHURT = 1;
+    public static final int FEY = 2; //trạng thái sắp chết
+    public static final int DEATH = 3;
+    public static final int NOBEHURT = 4;
+    private int state = ALIVE;
+    //biến xác định xem nhân vật thuộc trạng thái nào trong 5 trạng thái trên
+
+    //thông số chung của các đối tượng trong game
+    private float width;
+    private float height;
+    private float mass;
+    private float speedX;
+    private float speedY;
+    private int blood;
+
+    private int damage;
+
+    private int direction; //nhân giá trị LEFT_DIR hoặc RIGHT_DIR
+
+    protected Animation behurtForwardAnim, behurtBackAnim;
+    //các hiệu ứng mà mọi đối tượng đều có -> gộp để xử lý
+
+    private int teamType; //kiểu team ->thuộc team nào team chính hay phản diện
+
+    private long startTimeNoBeHurt; //thời gian bắt đầu miễn nhiễm sát thương
+    private long timeForNoBeHurt;  //khoảng thời gian miễn nhiễm sát thương
+
+    public ParticularObject(float x, float y, float width, float height, float mass, int blood, GameWorldState gameWorld){
+        super(x, y, gameWorld); //kế thừa GameObject
+        setWidth(width);
+        setHeight(height);
+        setMass(mass);
+        setBlood(blood);
+
+        direction = RIGHT_DIR;
+
+    }
+    //các hàm setter getter
+    public void setTimeForNoBehurt(long time){
+        timeForNoBeHurt = time;
+    }
+
+    public long getTimeForNoBeHurt(){
+        return timeForNoBeHurt;
+    }
+
+    public void setState(int state){
+        this.state = state;
+    }
+
+    public int getState(){
+        return state;
+    }
+
+    public void setDamage(int damage){
+        this.damage = damage;
+    }
+
+    public int getDamage(){
+        return damage;
+    }
+
+
+    public void setTeamType(int team){
+        teamType = team;
+    }
+
+    public int getTeamType(){
+        return teamType;
+    }
+
+    public void setMass(float mass){
+        this.mass = mass;
+    }
+
+    public float getMass(){
+        return mass;
+    }
+
+    public void setSpeedX(float speedX){
+        this.speedX = speedX;
+    }
+
+    public float getSpeedX(){
+        return speedX;
+    }
+
+    public void setSpeedY(float speedY){
+        this.speedY = speedY;
+    }
+
+    public float getSpeedY(){
+        return speedY;
+    }
+
+    public void setBlood(int blood){
+        if(blood>=0)
+            this.blood = blood;
+        else this.blood = 0;
+    }
+
+    public int getBlood(){
+        return blood;
+    }
+
+    public void setWidth(float width){
+        this.width = width;
+    }
+
+    public float getWidth(){
+        return width;
+    }
+
+    public void setHeight(float height){
+        this.height = height;
+    }
+
+    public float getHeight(){
+        return height;
+    }
+
+    public void setDirection(int dir){
+        direction = dir;
+    }
+
+    public int getDirection(){
+        return direction;
+    }
+
+    public abstract void attack();
+    //chưa biết đối tượng tấn công như nào nên để hàm abstract
+
+    public boolean isObjectOutOfCameraView(){
+        if(getPosX() - getGameWorld().camera.getPosX() > getGameWorld().camera.getWidthView() ||
+                getPosX() - getGameWorld().camera.getPosX() < -50
+                ||getPosY() - getGameWorld().camera.getPosY() > getGameWorld().camera.getHeightView()
+                ||getPosY() - getGameWorld().camera.getPosY() < -50)
+            return true;
+        else return false;
+    }
+
+    public Rectangle getBoundForCollisionWithMap(){
+        //vùng va chạm dùng để kiểm tra va chạm giữa các đối tượng trong không gian
+        Rectangle bound = new Rectangle();
+        bound.x = (int) (getPosX() - (getWidth()/2));
+        bound.y = (int) (getPosY() - (getHeight()/2));
+        bound.width = (int) getWidth();
+        bound.height = (int) getHeight();
+        return bound;
+    }
+
+    public void beHurt(int damgeEat){
+        //đối tượng bị mất máu dự theo số dam mà đối tượng nhận
+        setBlood(getBlood() - damgeEat);
+        state = BEHURT; //trạng thái chuyển sang bị đau
+        hurtingCallback(); //overwrite cho từng đối tượng cụ thể sau
+    }
+
+    @Override
+    public void Update(){
+        //các case trạng thái
+        switch(state){
+            case ALIVE:
+
+                // note: SET DAMAGE FOR OBJECT NO DAMAGE
+                ParticularObject object = getGameWorld().particularObjectManager.getCollisionWidthEnemyObject(this);
+                if(object!=null){
+
+                    if(object.getDamage() > 0){
+                        // switch state to fey if object die
+
+                        System.out.println("eat damage.... from collision with enemy........ "+object.getDamage());
+                        beHurt(object.getDamage());
+                    }
+
+                }
+
+
+
+                break;
+
+            case BEHURT:
+                if(behurtBackAnim == null){ //trường hợp kh có animation cho đối tượng
+                    state = NOBEHURT;
+                    startTimeNoBeHurt = System.nanoTime();
+                    if(getBlood() == 0)
+                        state = FEY;
+
+                } else {
+                    behurtForwardAnim.Update(System.nanoTime());
+                    if(behurtForwardAnim.isLastFrame()){ //frame cuối cùng của animation bị đau
+                        behurtForwardAnim.reset(); //reset animation
+                        state = NOBEHURT; //chuyển sang trạng thái miễn nhiễm
+                        if(getBlood() == 0) //nếu máu về 0 thì chuyển sang trạng thái sắp chết
+                            state = FEY;
+                        startTimeNoBeHurt = System.nanoTime();
+                    }
+                }
+
+                break;
+
+            case FEY:
+
+                state = DEATH;
+
+                break;
+
+            case DEATH:
+
+
+                break;
+
+            case NOBEHURT:
+                System.out.println("state = nobehurt");
+                if(System.nanoTime() - startTimeNoBeHurt > timeForNoBeHurt) //hết trạng thái miễn nhiễm
+                    state = ALIVE; //cập nhật trạng thái bình thường
+                break;
+        }
+
+    }
+
+    public void drawBoundForCollisionWithMap(Graphics2D g2){
+        Rectangle rect = getBoundForCollisionWithMap(); //va chạm với map
+        g2.setColor(Color.BLUE);
+        g2.drawRect(rect.x - (int) getGameWorld().camera.getPosX(), rect.y - (int) getGameWorld().camera.getPosY(), rect.width, rect.height);
+    }
+
+    public void drawBoundForCollisionWithEnemy(Graphics2D g2){
+        Rectangle rect = getBoundForCollisionWithEnemy(); //va chạm với enemy
+        g2.setColor(Color.RED);
+        g2.drawRect(rect.x - (int) getGameWorld().camera.getPosX(), rect.y - (int) getGameWorld().camera.getPosY(), rect.width, rect.height);
+    }
+
+    public abstract Rectangle getBoundForCollisionWithEnemy();
+
+    public abstract void draw(Graphics2D g2);
+
+    public void hurtingCallback(){};
+
+
+}
